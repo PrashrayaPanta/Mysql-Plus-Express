529136846e677dcd09181a51158ecf20eb8b54a0
Prashraya Panta <prashrayapanta33@gmail.com>
2025-04-09 23:46:30 +0545

    aa

--- db/connectDb.js
+++ db/connectDb.js
@@ -8,6 +8,7 @@ export const connectDB = async(req, res) =>{
     try{
         const db = await mysql.createConnection({
             host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
